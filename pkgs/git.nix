0c941fa398bfba5b2559395565b0a5df2e7e7b95
creator54 <hi.creator54@gmail.com>
2021-09-15 23:37:31 +0530

    cleanup

--- pkgs/git.nix
+++ pkgs/git.nix
@@ -5,17 +5,5 @@
     enable = true;
     userName = "creator54";
     userEmail = "hi.creator54@gmail.com";
-    aliases = { # Usage git i/c/b/r
-      i = "init";
-      c = "commit";
-      b = "branch";
-      r = "remote";
-      a = "add .";
-      l = "log";
-      ck = "checkout";
-      x = "reset --hard";
-      u = "pull origin $(git branch | sed 's/^* //')";
-      p = "push origin $(git branch | sed 's/^* //')";
-    };
   };
 }
