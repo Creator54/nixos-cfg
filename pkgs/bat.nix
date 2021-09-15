4ae7ef19eb4a2773f981d68e3c0749966a7889a7
creator54 <hi.creator54@gmail.com>
2021-09-16 00:18:35 +0530

    update bat config

--- pkgs/bat.nix
+++ pkgs/bat.nix
@@ -4,14 +4,9 @@
   home.packages = with pkgs.bat-extras; [ batgrep batman batwatch prettybat batdiff ];
   programs.bat = {
     enable = true;
-    #config = { theme = "Dracula"; };
-    #themes = {
-    #  dracula = builtins.readFile (pkgs.fetchFromGitHub {
-    #    owner = "dracula";
-    #    repo = "sublime"; # Bat uses sublime syntax for its themes
-    #    rev = "26c57ec282abcaa76e57e055f38432bd827ac34e";
-    #    sha256 = "019hfl4zbn4vm4154hh3bwk6hm7bdxbr1hdww83nabxwjn99ndhv";
-    #  } + "/Dracula.tmTheme");
-    #};
+    config = { 
+      theme = "zenburn";
+      style = "grid";
+    };
   };
 }
