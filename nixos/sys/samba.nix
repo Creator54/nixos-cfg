e22681b717f08ec9925329ee4e4f129a18894834
creator54 <hi.creator54@gmail.com>
2021-11-16 20:53:10 +0530

    fix file ownership, "guest ok" is same as "public"

--- nixos/sys/samba.nix
+++ nixos/sys/samba.nix
@@ -15,14 +15,14 @@
       private = { #create an useraccount and set password via "sudo smbpasswd -a username"
         path = "/run/mount/data1";
         "read only" = "no";
-        "create mask" = "0664"; #permissions 
+        "map archive" = "no"; #keep file owner, group
+        "create mask" = "0664";
         "force create mask" = "0664";
         "directory mask" = "0775";
         "force directory mode" = "0775";
         private = "yes";
         writable = "yes";
         browseable = "yes";
-        "guest ok" = "no";
         comment = "SMB: Local DATA Drive";
       };
     };
