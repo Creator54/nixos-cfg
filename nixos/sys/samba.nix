116e4a0818d9c0424cb9cc727d6cab3d3e5684dc
creator54 <hi.creator54@gmail.com>
2021-11-17 13:20:57 +0530

    update walli script, update dwm for walli keybind & setup a public SMB share

--- nixos/sys/samba.nix
+++ nixos/sys/samba.nix
@@ -12,6 +12,19 @@
     '';
     securityType = "user";
     shares = {
+      public = { #create an useraccount and set password via "sudo smbpasswd -a username"
+        path = "/home/creator54/shared";
+        "read only" = "no";
+        "map archive" = "no"; #keep file owner, group
+        "create mask" = "0664";
+        "force create mask" = "0664";
+        "directory mask" = "0775";
+        "force directory mode" = "0775";
+        public = "yes";
+        writable = "yes";
+        browseable = "yes";
+        comment = "For temp share";
+      };
       private = { #create an useraccount and set password via "sudo smbpasswd -a username"
         path = "/run/mount/data1";
         "read only" = "no";
