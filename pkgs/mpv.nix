026289f6d52cb13a4fa7331d09d47b7252c27f34
creator54 <hi.creator54@gmail.com>
2022-01-30 00:31:32 +0530

    add mpv options,update fish, connect BT/wifi onn login,random screenlock wall

--- pkgs/mpv.nix
+++ pkgs/mpv.nix
@@ -7,6 +7,10 @@
       vo="gpu";
       profile="gpu-hq";
       force-window = true;
+      fs="yes";
+      ontop = "yes";
+      osc = "no";
+      ytdl-format="bestvideo[height<=?480]+bestaudio/best";
     };
   };
 }
