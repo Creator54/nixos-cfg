715777a9b125c5d990fef520eed0c9104144e528
creator54 <hi.creator54@gmail.com>
2022-02-17 07:33:35 +0000

    add hardware,docker files

---
+++ docker.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, lib, ... }:
+
+{
+  #docker
+  virtualisation.docker = {
+    enable = true;
+    enableOnBoot = false;
+    #extraOptions = "--config-file=${pkgs.writeText "daemon.json" (builtins.toJSON {
+    #  graph ="/run/mount/data1/docker";
+    #  storage-driver = "overlay";
+    #})}";
+  };
+  users.users.creator54.extraGroups = ["docker"];
+  # https://stackoverflow.com/questions/24309526/how-to-change-the-docker-image-installation-directory
+  # https://github.com/NixOS/nixpkgs/issues/68349
+
+}
