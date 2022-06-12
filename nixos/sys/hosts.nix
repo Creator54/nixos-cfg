9e734da290da7a45056b9a31db5e0655181249c5
creator54 <hi.creator54@gmail.com>
2022-06-13 02:37:13 +0530

    shift to own fork

--- nixos/sys/hosts.nix
+++ nixos/sys/hosts.nix
@@ -3,7 +3,7 @@
     adawayHosts = builtins.readFile "${builtins.fetchurl https://adaway.org/hosts.txt}";
     adguardHosts = builtins.readFile "${builtins.fetchurl https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt}";
     stevenBlackHosts = builtins.readFile "${builtins.fetchurl https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts}";
-    youtubeAdBlockHosts = builtins.readFile "${builtins.fetchurl https://raw.githubusercontent.com/Ewpratten/youtube_ad_blocklist/master/blocklist.txt}";
+    youtubeAdBlockHosts = builtins.readFile "${builtins.fetchurl https://raw.githubusercontent.com/creator54/youtube_ad_blocklist/master/blocklist.txt}";
   in adawayHosts + adguardHosts + stevenBlackHosts + youtubeAdBlockHosts; #this reads them out together which gets captured and saved in extraHosts, use nix repl to learn.
 }
 
