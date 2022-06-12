27681b5c87512214aaf900a696d2d0910149fef1
creator54 <hi.creator54@gmail.com>
2022-06-13 01:49:18 +0530

    update hosts file with adguard,adaway hosts lists

--- nixos/sys/hosts.nix
+++ nixos/sys/hosts.nix
@@ -1,7 +1,8 @@
 {
   networking.extraHosts = let
-    hostsPath = https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts;
-    hostsFile = builtins.fetchurl hostsPath;
-  in builtins.readFile "${hostsFile}";
+    adawayHosts = builtins.readFile "${builtins.fetchurl https://adaway.org/hosts.txt}";
+    adguardHosts = builtins.readFile "${builtins.fetchurl https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt}";
+    stevenBlackHosts = builtins.readFile "${builtins.fetchurl https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts}";
+  in adawayHosts + adguardHosts + stevenBlackHosts; #this reads them out together which gets captured and saved in extraHosts, use nix repl to learn.
 }
 
