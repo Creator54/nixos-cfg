23422d23b9709980b96c6887ec31f3c97f96c22a
creator54 <hi.creator54@gmail.com>
2022-06-13 01:59:26 +0530

    add ytAdBlockHosts

--- nixos/sys/hosts.nix
+++ nixos/sys/hosts.nix
@@ -3,6 +3,7 @@
     adawayHosts = builtins.readFile "${builtins.fetchurl https://adaway.org/hosts.txt}";
     adguardHosts = builtins.readFile "${builtins.fetchurl https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt}";
     stevenBlackHosts = builtins.readFile "${builtins.fetchurl https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts}";
-  in adawayHosts + adguardHosts + stevenBlackHosts; #this reads them out together which gets captured and saved in extraHosts, use nix repl to learn.
+    youtubeAdBlockHosts = builtins.readFile "${builtins.fetchurl https://raw.githubusercontent.com/Ewpratten/youtube_ad_blocklist/master/blocklist.txt}";
+  in adawayHosts + adguardHosts + stevenBlackHosts + youtubeAdBlockHosts; #this reads them out together which gets captured and saved in extraHosts, use nix repl to learn.
 }
 
