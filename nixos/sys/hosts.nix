7f65991277e644f05168cebbab2add9d3eec26f0
creator54 <hi.creator54@gmail.com>
2022-07-09 18:49:37 +0530

    better syntax

--- nixos/sys/hosts.nix
+++ nixos/sys/hosts.nix
@@ -1,9 +1,9 @@
 {
-  networking.extraHosts = let
-    adawayHosts = builtins.readFile "${builtins.fetchurl https://adaway.org/hosts.txt}";
-    adguardHosts = builtins.readFile "${builtins.fetchurl https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt}";
-    stevenBlackHosts = builtins.readFile "${builtins.fetchurl https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts}";
-    youtubeAdBlockHosts = builtins.readFile "${builtins.fetchurl https://raw.githubusercontent.com/creator54/youtube_ad_blocklist/master/blocklist.txt}";
-  in adawayHosts + adguardHosts + stevenBlackHosts + youtubeAdBlockHosts; #this reads them out together which gets captured and saved in extraHosts, use nix repl to learn.
+  networking.hostFiles = [
+    (builtins.fetchurl https://adaway.org/hosts.txt)
+    (builtins.fetchurl https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt)
+    (builtins.fetchurl https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts)
+    (builtins.fetchurl https://raw.githubusercontent.com/creator54/youtube_ad_blocklist/master/blocklist.txt)
+  ];
 }
 
