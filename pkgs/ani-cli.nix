30cced29d5b15402a43a7704a5fb0d97f15c7239
creator54 <hi.creator54@gmail.com>
2022-07-11 02:10:50 +0530

    update: ani-cli 3.2.2

--- pkgs/ani-cli.nix
+++ pkgs/ani-cli.nix
@@ -1,12 +1,12 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   home.packages = [
     (pkgs.fetchFromGitHub {
       owner = "pystardust";
       repo = "ani-cli";
-      rev = "v3.2";
-      sha256 = "sha256-LSyqcJX+bPW/qtO3FaZh9RaXW2xeGc/9tdC2CmhDwUw=";
+      rev = "3da894148b395078c3e8673e5a6535dfb215f893"; #3.2.2
+      sha256 = "sha256-rtoQUJsvQzWqrOIC4vhtfdSvgfbl9BXn++TddngtS08=";#lib.fakeSha256;
     })
   ];
 }
