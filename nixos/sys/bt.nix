02ef3267c3f0200b79d927618775a38e37cda07b
creator54 <hi.creator54@gmail.com>
2022-07-23 02:24:17 +0530

    update bt : enable hsphfpd for bt controls

--- nixos/sys/bt.nix
+++ nixos/sys/bt.nix
@@ -4,11 +4,12 @@
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true; #doesnt work on false
+    hsphfpd.enable = true; #for bluetooth controls
     settings.General.Enable = "Source,Sink,Media,Socket";
   };
   services.blueman.enable = true;
   # currently need to
   # sudo rfkill unblock bluetooth;sudo systemctl restart bluetooth;
-  # now connect via `headset` or bluetoothctl/blueman-applet 
+  # now connect via `headset` or bluetoothctl/blueman-applet
 }
- 
+
