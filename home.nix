b555ee4b854bc1c2b4c53c744a4fbe09b52bd8dd
creator54 <hi.creator54@gmail.com>
2022-07-29 11:23:29 +0530

    revert to using shell config for adding to path not working via home-manager

--- home.nix
+++ home.nix
@@ -13,7 +13,6 @@
     stateVersion = "22.05"; #do not touch
     homeDirectory = "/home/creator54";
     enableNixpkgsReleaseCheck = true;
-    sessionPath = [ "$HOME/.node_modules/bin" ];
 
     file = {
       ".config/gromit-mpx.ini".source                       = ./configs/gromit-mpx.ini;
