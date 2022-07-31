522745559f5dcfd8bade49d6379792ebc3fa79be
creator54 <hi.creator54@gmail.com>
2022-07-31 22:11:38 +0530

    enable kdeconnect-applet , add xplr configs

--- home.nix
+++ home.nix
@@ -18,6 +18,7 @@
       ".config/gromit-mpx.ini".source                       = ./configs/gromit-mpx.ini;
       ".config/flameshot".source                            = ./configs/flameshot;
       ".config/fish".source                                 = ./configs/fish;
+      ".config/xplr".source                                 = ./configs/xplr;
       ".config/mpv/scripts".source                          = ./configs/mpv/scripts;
       ".config/mpv/script-opts/youtube-quality.conf".source = ./configs/mpv/youtube-quality.conf;
       ".config/gtk-3.0".source                              = ./configs/gtk-3.0;
