45853675e58dee9d87369ac4a89c2e77af732028
creator54 <hi.creator54@gmail.com>
2022-08-25 02:05:01 +0530

    dynamic paths -> local if exists else fetchTarball

--- wm/dwm.nix
+++ wm/dwm.nix
@@ -1,7 +1,9 @@
 { pkgs, lib, ... }:
 let
+  localDwm = /home/creator54/dwm;
+  localDwmBlocks = /home/creator54/dwmblocks;
   dwm = pkgs.dwm.overrideAttrs (old: {
-    src = builtins.fetchTarball https://github.com/creator54/dwm/tarball/main;
+    src = if builtins.pathExists localDwm then localDwm else builtins.fetchTarball https://github.com/creator54/dwm/tarball/main;
     nativeBuildInputs = with pkgs; [ #writing once works for both currently, sort of bug and feature
       xorg.libX11.dev
       xorg.libXft
@@ -10,7 +12,7 @@ let
     ];
   });
   dwmblocks = pkgs.dwmblocks.overrideAttrs (old: {
-    src = builtins.fetchTarball https://github.com/creator54/dwmblocks/tarball/master;
+    src = if builtins.pathExists localDwmBlocks then localDwmBlocks else builtins.fetchTarball https://github.com/creator54/dwmblocks/tarball/master;
   });
 in
 {
