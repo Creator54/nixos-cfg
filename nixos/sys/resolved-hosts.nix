33816efca2bacc0d8df4888ae3dfe32d64aaf2cd
creator54 <hi.creator54@gmail.com>
2022-08-25 00:31:19 +0530

    add more resolved hosts

--- nixos/sys/resolved-hosts.nix
+++ nixos/sys/resolved-hosts.nix
@@ -1,8 +1,10 @@
 {
   networking.extraHosts = ''
+    quora.com www.quora.com
     142.250.182.196 google.in
     142.250.182.196 google.com
     142.250.182.196 google.co.in
+    20.207.73.82 github.com
     13.234.176.102 github.com
     172.217.167.174 youtube.com
     163.53.78.110 flipkart.com
