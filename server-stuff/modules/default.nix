32fb8aa9ec8f28dccc958fa48cb898460dbc6db7
creator54 <hi.creator54@gmail.com>
2022-11-22 10:35:24 +0000

    web -> config

--- server-stuff/modules/default.nix
+++ server-stuff/modules/default.nix
@@ -1,14 +1,15 @@
 let
-  web = (import ../../userConfig.nix).userConfig.web;
+  config = (import ../../userConfig.nix).userConfig;
 in
 {
   imports = [
-    (if web.enable then ./web.nix else ./place-holder.nix)
-    (if web.whoogle.enable then ./whoogle.nix else ./place-holder.nix)
-    (if web.adguard.enable then ./adguard.nix else ./place-holder.nix)
-    (if web.plex.enable then ./plex-server.nix else ./place-holder.nix)
-    (if web.jellyfin.enable then ./jellyfin.nix else ./place-holder.nix)
-    (if web.nextCloud.enable then ./next-cloud.nix else ./place-holder.nix)
-    (if web.codeServer.enable then ./code-server.nix else ./place-holder.nix)
+    (if config.docker.enable then ./docker.nix else ./place-holder.nix)
+    (if config.web.enable then ./web.nix else ./place-holder.nix)
+    (if config.web.whoogle.enable then ./whoogle.nix else ./place-holder.nix)
+    (if config.web.adguard.enable then ./adguard.nix else ./place-holder.nix)
+    (if config.web.plex.enable then ./plex-server.nix else ./place-holder.nix)
+    (if config.web.jellyfin.enable then ./jellyfin.nix else ./place-holder.nix)
+    (if config.web.nextCloud.enable then ./next-cloud.nix else ./place-holder.nix)
+    (if config.web.codeServer.enable then ./code-server.nix else ./place-holder.nix)
   ];
 }
