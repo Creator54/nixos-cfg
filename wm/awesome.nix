9b58302d7fd2b52d8ee6dd3548d19cfccaf46808
creator54 <hi.creator54@gmail.com>
2022-12-11 13:09:55 +0530

    update flake, awesome

--- wm/awesome.nix
+++ wm/awesome.nix
@@ -1,6 +1,9 @@
 { config, pkgs, lib, ... }:
 
+let
+  awesome = /home/creator54/awesome;
+in
 {
   xsession.windowManager.awesome.enable = true;
-  home.file.".config/awesome".source = ./wm-configs/awesome;
+  home.file.".config/awesome".source = if builtins.pathExists awesome then awesome else builtins.fetchTarball "https://github.com/creator54/awesome/tarball/new";
 }
