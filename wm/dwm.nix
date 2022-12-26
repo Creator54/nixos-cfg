46a307f5efe9f35b814eb200d763cdc5d89c7672
creator54 <hi.creator54@gmail.com>
2022-12-27 00:17:42 +0530

    update dwm, cleanup

--- wm/dwm.nix
+++ wm/dwm.nix
@@ -3,7 +3,7 @@ let
   localDwm = /home/creator54/dwm;
   localDwmBlocks = /home/creator54/dwmblocks;
   dwm = pkgs.dwm.overrideAttrs (old: {
-    src = if builtins.pathExists localDwm then localDwm else builtins.fetchTarball https://github.com/creator54/dwm/tarball/main;
+    src = if builtins.pathExists localDwm then localDwm else builtins.fetchTarball "https://github.com/creator54/dwm/tarball/main";
     nativeBuildInputs = with pkgs; [ #writing once works for both currently, sort of bug and feature
       xorg.libX11.dev
       xorg.libXft
@@ -12,7 +12,7 @@ let
     ];
   });
   dwmblocks = pkgs.dwmblocks.overrideAttrs (old: {
-    src = if builtins.pathExists localDwmBlocks then localDwmBlocks else builtins.fetchTarball https://github.com/creator54/dwmblocks/tarball/master;
+    src = if builtins.pathExists localDwmBlocks then localDwmBlocks else builtins.fetchTarball "https://github.com/creator54/dwmblocks/tarball/master";
   });
 in
 {
