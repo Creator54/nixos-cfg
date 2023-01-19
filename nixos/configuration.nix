ac4e02f670437bbef7fed5acca063a9e5e952b97
creator54 <hi.creator54@gmail.com>
2023-01-19 20:59:41 +0530

    update locale

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -52,7 +52,7 @@
   time.timeZone = "Asia/Kolkata";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_IN.UTF-8";
+  i18n.defaultLocale = "en_US.UTF-8";
   #console = {
   #  earlySetup = true;
   #  font = "${pkgs.terminus_font}/share/consolefonts/ter-v18n.psf.gz"; #setfont ter-132b
