fbc059e5743710df6d1208a6da7034c56b103b21
creator54 <hi.creator54@gmail.com>
2023-04-17 21:59:33 +0000

    cleanup

--- userConfig.nix
+++ userConfig.nix
@@ -14,8 +14,9 @@ in
     hostSrc = builtins.fetchTarball https://github.com/creator54/creator54.me/tarball/main;
     blogSrc = builtins.fetchTarball https://github.com/creator54/blog.creator54.me/tarball/main + "/_site/";
 
+    docker.enable = false;
     web = {
-      enable = true; #enables website,blog,docs,test
+      enable = false; #enables website,blog,docs,test
       codeServer = {
         enable = false;
         host = "code.${userDomain}";
@@ -23,17 +24,17 @@ in
         port = 5000;
       };
       nextCloud = {
-        enable = true;
+        enable = false;
         adminUser = "${user}";
         host = "cloud.${userDomain}";
       };
       adguard = {
-        enable = true;
+        enable = false;
         host = "ag.${userDomain}";
         port = 3000; #set this port only in the webUI
       };
       jellyfin = {
-        enable = true;
+        enable = false;
         user = "${user}";
         host = "tv.${userDomain}";
         port = 8096; #jellyfin runs on this port by default, no option to change currently
@@ -46,16 +47,15 @@ in
         port = 32400; #plex runs on this port by default, no option to change currently
       };
       whoogle = {
-        enable = true;
+        enable = false;
         host = "search.${userDomain}";
         port = "8050"; #strings allowed here
       };
     };
 
     sshKeys = [
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDUG+dQCoGoq1cHa6yYmx925i1GJqgFY/+NRh9mnecxzNiqm4BdsBz/wfn7WKJzGjQNKb5QX3QD72xJHXojziFhVA531rJoQavM93IKDI1vlng7v4mHnQVMQqT6l67zZDEraVs2/V3Rz+5YYo2ecvRhY4i5evzR8t2h89jWVYjUHcgGQPwnHzFjLItI7rnaYS50QaeCgfpRMqjwosp/FDKZYHz+gCMTOH6l5wFkmRJBBWviwVybwDC3Hpp88hS3DrC/HrFrOpzhFzCIs18RxefEEp0EDAIf5pS5gbHgIfkuX4SQTIWA2ZOeHTMDQrOx12EsqSccFxB0eczdtu/MoOBh webserver"
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDHSEw5zX0noyIHiLM3DuNsHaSKP/1wiwKJQWPBGzOgALwarc6HFW5kY1uzS6YFAyVtMG8jh3AsGdEV4V3p8rvFdyupxvC0lkFWI3eq2JI7AZRjSZyPmwUDo1OGDUCyK2nMg5O4vXOMWN5TG5z2CduOWkpKMRK7AF5jN5cIYhMmg9eYMjxAfppYPrfdOr8YRsadNJs8MtFwtwLn4jFpc781uw8zo9oyLZQ9knja0EgXrDSA6SGo+nQNMEbogU2xxVD18WfwoucHm+vw+DuBWQcMMHqXvfdv4KVatNBQt0V3XNdax0nRT2a6F4f/L6XAgUs2pQRgold7lN42RGiyBV6RZd/RAPvhdSU5kHcft7B1wCUvHivwH2DQlQs76H+52u456ruytkjaqpS7A6QHCAUdmP9RHuz85f9g/qQuVpmsW3u1pphTilt786UHGv1aEkxAI1AN/irK6DO7GAJEKkf1OuaEuKYIRzRkhRUSjEVYQb26v4v4O5ZPc5duWFUY8ZWinBTZ7SWcSeFE6gxuDx6UTVki/UKGyHJsU3sJl80ma9YoGYN0P4BfXVycUO4kgQGrSDbu6uXQ41vjiYXr/tLosSLtoXOwmO5K5nfFTCH/CCtWEgqKOn/8avZ4mV6UjIBlQE38GLu4Yq7622V4aCxCJpUC2IVteJRPabWmra4kQQ== hi.creator54@gmail.com"
     ];
-    stateVersion = "22.05";
+    stateVersion = "22.11";
   };
 }
